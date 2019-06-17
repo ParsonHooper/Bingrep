0154143f354f43a539d1840e03e89084e091d8a1
Parson Hooper <dps9091@gmail.com>
2019-06-17 18:02:33 -0400

    Initial commit

---
+++ bingrep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+bingrep () {
+  if [ $# = 1 ] && [ $1 = '-h' -o $1 = '-help' -o $1 = '--help' ]
+  then echo "Usage: bingrep [searchRegex] [searchFile]" && return
+  elif [ $# -lt 2 ]
+  then echo "ERROR: Too few arguments" && echo "Usage: bingrep [searchRegex] [searchFile]" && return
+  elif [ $# -gt 2 ]
+  then echo "ERROR: Too many arguments" && echo "Usage: bingrep [searchRegex] [searchFile]" && return
+  else grep -bor --binary-files=text $1 $2
+  fi
+}
